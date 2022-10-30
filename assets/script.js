dc64b19000d00c78c2b7d747442a7b7145da1b6c
MandieBot <amandabotinelly@gmail.com>
2022-10-29 18:17:59 -0600

    Created variables and functions

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,112 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Declare variables
+var lowerArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var charactersArray = ["!", "@", "#", "$", "*", "%"];
+var lowerCase;
+var upperCase;
+var numericValue;
+var specialChar;
+var pwLength;
+
+//Length prompt
+var getLength = function () {
+  var pwLength = window.prompt("How long do you want your password?");
+  return pwLength;
+  // lowerCase = getLowercase();
+};
+
+var getLowercase = function () {
+  var lowercaseOpt = window.confirm(
+    "Do you want to include lowercase characters?"
+  );
+  return lowercaseOpt;
+};
+
+var getUppercase = function () {
+  var uppercaseOpt = window.confirm(
+    "Do you want to include uppercase characters?"
+  );
+  return uppercaseOpt;
+};
+var getNumericValue = function () {
+  var numericOpt = window.confirm("Do you want to include numeric values?");
+  return numericOpt;
+};
+var getSpecialChar = function () {
+  var specialOpt = window.confirm("Do you want to include special characters?");
+  return specialOpt;
+};
+
+pwLength = getLength();
+
+if (pwLength >= 8 && pwLength <= 128) {
+  lowerCase = getLowercase();
+  upperCase = getUppercase();
+  numericValue = getNumericValue();
+  specialChar = getSpecialChar();
+} else {
+  alert("Please choose a number between 8 and 128");
+}
